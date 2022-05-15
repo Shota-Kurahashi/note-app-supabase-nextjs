706321ca12e97a03f4658e633b7e97aeb30f82f4
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-05-15 19:50:11 +0900

    updated

---
+++ pages/api/notes-edge/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextApiRequest, NextApiResponse } from "next";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function handler(req: NextApiRequest, res: NextApiResponse) {}
